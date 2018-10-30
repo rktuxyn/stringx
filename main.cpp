74345b6eea0642b54e9aab541d6722f525f0bd00
aBnoRmaL pRogRamMer <mssclang@outlook.com>
2018-10-31 00:54:30 +0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,3 +1,9 @@
+/**
+* Copyright (c) 2018, SOW (https://www.facebook.com/safeonlineworld). (https://github.com/RKTUXYN) All rights reserved.
+* @author {SOW}
+* Copyrights licensed under the New BSD License.
+* See the accompanying LICENSE file for terms.
+*/
 #include "stringx"
 int
  main() {
